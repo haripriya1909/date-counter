0a62ab256a0245cdaa7548cc90e7b5c3b3a57840
hari1909 <hari19september@gmail.com>
2024-05-27 19:53:57 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import { useState } from "react";
 
-import useState from "react";
 export default function App() {
-  return (
-    <>
-      <Task />
-      <Task />
-      <div>
-       from today is
-      </div>
-    </>
-  );
-}
-function Task() {
-  const[count , setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [step, setStep] = useState(0);
+  const date = new Date("may 27 2024");
+  date.setDate(date.getDate() + count);
+
   return (
     <div className="task">
-      <button className="btn" onClick={() => {setCount(count-1)}}>-</button>
-      <p>Step:{count}</p>
-      <button className="btn" onClick={() => {setCount(count+1)}}>+</button>
+      <button
+        className="btn"
+        onClick={() => {
+          setStep(step - 1);
+        }}>
+        -
+      </button>
+      <span>Step:{step}</span>
+      <button
+        className="btn"
+        onClick={() => {
+          setStep(step + 1);
+        }}>
+        +
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          setCount(count - step);
+        }}>
+        -
+      </button>
+      <span>Count:{count}</span>
+      <button
+        className="btn"
+        onClick={() => {
+          setCount(count + step);
+        }}>
+        +
+      </button>
+
+      <span>
+        {count === 0
+          ? "Today is"
+          : count > 0
+          ? `${count} days from today is `
+          : `${Math.abs(count)} days ago was `}
+        {date.toDateString()}
+      </span>
     </div>
   );
 }
